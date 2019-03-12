e47579692b7e5c229dc3e3dfb6ef96c47045ddb2
ajian <idengjian@163.com>
2019-03-12 15:26:31 +0800

    添加基本文件夹目录

--- app.js
+++ app.js
@@ -23,13 +23,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// app.get('/data',  function(req, res, next) {
+//   res.send('data')
+// })
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) { 
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
